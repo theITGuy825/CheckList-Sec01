d1e676dfa65541a5b55ebe45b1caf1ee4664cb5e
theITGuy825 <e_henderson02@outlook.com>
2025-01-22 10:28:51 -0500

    Added Firebase

--- app.js
+++ app.js
@@ -2,16 +2,47 @@ const taskInput = document.getElementById('taskInput');
 const addTaskBtn = document.getElementById('addTaskBtn');
 const taskList = document.getElementById('taskList');
 
-// Add Task
-addTaskBtn.addEventListener('click', () => {
+addTaskBtn.addEventListener('click', async () => {
     const task = taskInput.value.trim();
     if (task) {
-        const li = document.createElement('li');
-        li.textContent = task;
-        taskList.appendChild(li);
-        taskInput.value = '';
+    const taskInput = document.getElementById("taskInput");
+    const taskText = taskInput.value.trim();
+    if (taskText) {
+    await addTaskToFirestore(taskText);
+    renderTasks();
+    taskInput.value = "";
     }
-});
+    renderTasks();
+    }
+    });
+    async function addTaskToFirestore(taskText) {
+    await addDoc(collection(db, "todos"), {
+    text: taskText,
+    completed: false
+    });
+    }
+
+    async function renderTasks() {
+        var tasks = await getTasksFromFirestore();
+        taskList.innerHTML = "";
+        tasks.forEach((task, index) => {
+        if(!task.data().completed){
+        const taskItem = document.createElement("li");
+        taskItem.id = task.id;
+        taskItem.textContent = task.data().text;
+        taskList.appendChild(taskItem);
+        }
+        });
+        }
+        async function getTasksFromFirestore() {
+        var data = await getDocs(collection(db, "todos"));
+        let userData = [];
+        data.forEach((doc) => {
+        userData.push(doc);
+        });
+        return userData;
+        }
+
 
 // Remove Task on Click
 taskList.addEventListener('click', (e) => {
@@ -19,3 +50,40 @@ taskList.addEventListener('click', (e) => {
         e.target.remove();
     }
 });
+
+const sw = new URL('service-worker.js', import.meta.url)
+if ('serviceWorker' in navigator) {
+    const s = navigator.serviceWorker;
+    s.register(sw.href, {
+        scope: '/Lab1-2/'
+    })
+        .then(_ => console.log('Service Worker Registered for scope:', sw.href,
+'with', import.meta.url))
+        .catch(err => console.error('Service Worker Error:', err));
+}
+
+const taskText = sanitizeInput(taskInput.value.trim());
+
+import { initializeApp } from "firebase/app";
+import { doc, getDocs, addDoc, updateDoc, getFirestore, collection } from
+"firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDhpku4RzYDq6Sf6_HsEhn8bgePZeetv9A",
+  authDomain: "webdevtrends-31a7c.firebaseapp.com",
+  projectId: "webdevtrends-31a7c",
+  storageBucket: "webdevtrends-31a7c.firebasestorage.app",
+  messagingSenderId: "791440809739",
+  appId: "1:791440809739:web:721bd554b36c47eee58c0e"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+function sanitizeInput(input) {
+    const div = document.createElement("div");
+    div.textContent = input;
+    return div.innerHTML;
+    }
+
+    
