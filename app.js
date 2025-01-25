ee70402b1cbd16c35aec8439f78a70a5e5a23171
theITGuy825 <e_henderson02@outlook.com>
2025-01-25 12:44:32 -0500

    fixed app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const sw = new URL('service-worker.js', import.meta.url)
 if ('serviceWorker' in navigator) {
     const s = navigator.serviceWorker;
     s.register(sw.href, {
-        scope: '/CheckList/'
+        scope: '/Lab1-2/'
     })
         .then(_ => console.log('Service Worker Registered for scope:', sw.href, 'with', import.meta.url))
         .catch(err => console.error('Service Worker Error:', err));
