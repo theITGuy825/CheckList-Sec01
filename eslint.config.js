d56b18d9606d10313ec4620ebe0cccc088c241d8
theITGuy825 <theITGuy825@users.noreply.github.com>
2025-02-20 03:44:18 +0000

    deploy: 1b1d07694706966eb51bac3fe4d48c949e50f6d1

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,7 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import js from "@eslint/js";
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    rules: {
-    "no-unused-vars": "warn",
-    "no-undef": "warn"
-    },
-    files: ["*.js"]
-    },
   {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
 ];
