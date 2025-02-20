e386c1e308c32848d284e2fe4f13308d01dc8e8b
theITGuy825 <e_henderson02@outlook.com>
2025-02-19 21:23:17 -0500

    added fbjs

--- service-worker.js
+++ service-worker.js
@@ -4,6 +4,7 @@ const FILES_TO_CACHE = [
  '/CheckList-Sec01/index.html',
  '/CheckList-Sec01/style.css',
  '/CheckList-Sec01/app.js',
+ '/CheckList-Sec01/firebase-config.js',
  '/CheckList-Sec01/manifest.json',
  '/CheckList-Sec01/icons/icon-128.png',
  '/CheckList-Sec01/icons/icon-512.png'
